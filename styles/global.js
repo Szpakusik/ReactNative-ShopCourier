a7701cc628046d55c32b03256c390f1f681716a4
Szpakusik <szpakusik@gmail.com>
2020-10-15 19:25:24 +0200

    Initial styles

---
+++ styles/global.js
@@ -0,0 +1,17 @@
+import { AppLoading } from 'expo'
+import { StyleSheet } from 'react-native'
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        padding: 20,
+        flex: 1
+    },
+    input:{
+        borderWidth: 1,
+        color:'#ddd',
+        padding: 10,
+        fontSize: 18,
+        borderRadius: 6,
+        margin: 5
+    }
+})
