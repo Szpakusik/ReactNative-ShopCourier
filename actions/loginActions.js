fbf05771166636acd1c5a61561c828d547d729ea
Szpakusik <szpakusik@gmail.com>
2021-02-04 12:37:51 +0100

    Secure store JWT

--- actions/loginActions.js
+++ actions/loginActions.js
@@ -1,5 +1,29 @@
-export const loginAction = ( isLogged ) => {
+import axios from "axios"
+import * as SecureStore from 'expo-secure-store';
+import { serverUrl } from '../assets/urls'
+
+const setToken = (token) => {
+    return SecureStore.setItemAsync('secure_token', token);
+};
+
+export const loginAction = ( isLogged, email, password ) => {
     return ( dispatch ) => {
-        dispatch( { type: "LOG_IN", payload: { isLogged: isLogged } } )
+
+        axios.post( serverUrl + '/account/login', {
+            username: "qwe",
+            password: "qwe",
+        })
+        .then(function (res) {
+            
+            dispatch( { type: "LOG_IN", payload: { isLogged: false } } )
+            setToken( res.data.token );
+
+        })
+        .catch(function (error) {
+
+            console.log(error);
+            // setMessage( 'Błąd logowania, spróbuj ponownie' );
+        });
+
     }
 }
